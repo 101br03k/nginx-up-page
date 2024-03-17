5df9c773482501c27256dd281eedaf7428b2ee2c
101br03k <warmerdamm03@gmail.com>
2024-03-17 17:40:13 +0000

    updated nginx build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose down
-docker image rm ghcr.io/101br03k/nginx-up-page:v1.0-nv-1.10.1-alpine
-docker build . -t ghcr.io/101br03k/nginx-up-page:v1.0-nv-1.10.1-alpine
+docker image rm ghcr.io/101br03k/nginx-up-page:v1.1-nv-stable-alpine3.17
+docker build . -t ghcr.io/101br03k/nginx-up-page:v1.1-nv-stable-alpine3.17
 docker-compose up -d
